cf57a2ab3fb88c879f050715713b20f54cf3ed36
Jorge Angel Lopez Corona <jorgeangellopezcorona@Jorges-MacBook-Pro.local>
2020-07-08 12:46:40 -0500

    Upgrade Micronaut 1->2
    Solve Data method error
    Updating new dependencies

---
+++ app/Readme.md
@@ -0,0 +1,3 @@
+https://hub.docker.com/_/postgres/
+
+docker run --rm -p 32768:5432/tcp --name postgres03_00 postgres03
--- app/src/main/java/com/micronaut/docker/entity/Airport.java
+++ app/src/main/java/com/micronaut/docker/entity/Airport.java
@@ -1,12 +1,15 @@
 package com.micronaut.docker.entity;
 
-import lombok.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-@Data
+//@Data
 @RequiredArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -14,6 +17,30 @@ public class Airport {
 
     {
         System.out.println("Bean "+this.getClass().getName());
+    }
+
+    public String getAirportCd() {
+        return airportCd;
+    }
+
+    public void setAirportCd(String airportCd) {
+        this.airportCd = airportCd;
+    }
+
+    public String getAirportName() {
+        return airportName;
+    }
+
+    public void setAirportName(String airportName) {
+        this.airportName = airportName;
+    }
+
+    public String getAirportCty() {
+        return airportCty;
+    }
+
+    public void setAirportCty(String airportCty) {
+        this.airportCty = airportCty;
     }
 
     @Id
